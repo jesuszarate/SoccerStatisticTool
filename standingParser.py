834dcba32a385c002f2746c7be7711907c9d4468
Jesus <chuy8jay@gmail.com>
2016-03-03 15:24:34 -0700

    Trying to get parse to work

---
+++ standingParser.py
@@ -0,0 +1,22 @@
+import json
+with open('LigaMXStadings2016.txt', 'r') as standings:
+    teams = []
+    for team in standings:
+        teams.append(team)
+
+# Each number store as variable
+for team in teams:
+    for element in team.split('\t'):
+        team_name = ''
+        standing = []
+        try:
+            #print element
+            int(element.strip())
+            standing.append(element)
+        except ValueError:
+            print "Could not convert data to an integer."
+
+    print json.dumps(team_name)
+
+# Print what it would look like in json
+
