d8dc65e0f64acd6ee8a76a046124fa26c01f4376
Jesus <chuy8jay@gmail.com>
2016-08-07 21:27:28 -0600

    Cleaned up and moved database stuff to database dir

--- espnParser.py
+++ espnParser.py
@@ -1,22 +1,38 @@
 from lxml import html
 from bs4 import BeautifulSoup
+from mysql.connector import Error
 import requests
+import argparse
 
 
-#url = 'http://www.univision.com/deportes/futbol/liga-mx-apertura/calendario'
-url = 'http://espndeportes.espn.com/futbol/calendario/_/liga/mex.1'
-url = 'http://espndeportes.espn.com/futbol/fixtures/_/fecha/20160729/liga/mex.1'
+def parseDate(date):
+    darr = date.split('/')
+    return darr[2] + darr[0] + darr[1]
 
-r = requests.get(url)
+def parse(date):
+    # TODO: Make the league interchangable
+    url = 'http://espndeportes.espn.com/futbol/fixtures/_/fecha/' + date + '/liga/mex.1'
+    print 'fetching data from...'
+    print url
 
-soup = BeautifulSoup(r.content, "lxml")
+    r = requests.get(url)
 
-lines = soup.find_all("tr", {"class":["odd","even"]})
+    soup = BeautifulSoup(r.content, "lxml")
 
-#print soup.prettify
-for line in lines:
-    print line.contents[0].find_all("a", {"class":"team-name"})[0].find_all("span")[0].text + ' ' + \
-    line.contents[0].find_all("span", {"class":"record"})[0].find_all("a")[0].text + ' ' + \
-    line.contents[1].find_all("a", {"class":"team-name"})[0].find_all("span")[0].text
+    lines = soup.find_all("tr", {"class":["odd","even"]})
+
+    for line in lines:
+        print line.contents[0].find_all("a", {"class":"team-name"})[0].find_all("span")[0].text + ' ' + \
+            line.contents[0].find_all("span", {"class":"record"})[0].find_all("a")[0].text + ' ' + \
+            line.contents[1].find_all("a", {"class":"team-name"})[0].find_all("span")[0].text
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("date", help="Date of the page you want parsed, is the following format mm/dd/yyyy",
+                    type=str)
+args = parser.parse_args()
+
+print args.date
+parse(parseDate(args.date))
+
+connect()
