7f99f781f9021ced06374fb95e3c3f017f338680
Jesus <chuy8jay@gmail.com>
2016-08-12 04:00:40 -0600

    Able to update weekly predictions

--- recordGamesToDatabase.py
+++ recordGamesToDatabase.py
@@ -18,7 +18,8 @@ os.chdir(os.getcwd() + '/database') # Must change current working direcory to wh
 
 def saveMatches():
     print '\n\n\n\n'
-    for o in parse(args.date):
+    matches = parse(args.date)
+    for o in matches:
         homeId =  getTeamId(o['home']['name'])
         homeScore = o['home']['score']
         awayId =  getTeamId(o['away']['name'])
@@ -30,7 +31,9 @@ def saveMatches():
 
         date = formatDate(args.date)
         insertMatch(homeId, homeScore, awayId, awayScore, winner, date)
-        recordRacha(date);        
+
+        #TODO: Uncomment this when time is right
+        #recordRacha(date);        
 
 
 # Records racha for all games in database
@@ -43,6 +46,39 @@ def recordRacha(date):
         # parameters matchId, currentTeam, win/lose/tie, date
         db.insertRachaEntry(match[0], match[1], compareMatchResults(match[2], match[4], results)[0], date)
         db.insertRachaEntry(match[0], match[3], compareMatchResults(match[4], match[2], results)[0], date)
+
+
+def recordPredictions(fileName, date):
+    matches = parsePredictionFile(fileName)
+    
+    for match in matches:
+        homeId = getTeamId(match[0])
+        awayId = getTeamId(match[1])
+        prediction = db.getPredictionId(match[2])
+
+        print 'prediction ' + match[2]
+        print 'home ' + str(homeId)
+        print 'away ' + str(awayId)
+        print 'predict ' + str(prediction)
+
+        matchId = db.getMatchId(homeId, awayId, date)
+
+        if matchId is not None:
+            db.insertPrediction(matchId, prediction)
+        else:
+            print 'Error: No match is recorded for the date provided.'
+
+def parsePredictionFile(fileName):
+    array = []
+    with open(fileName) as f:
+
+        for line in f:
+            print line
+            c = line.decode('utf8')
+            cont = c.split(',')
+            array.append((cont[0], cont[2], cont[3].rstrip()))
+
+    return array
 
 # Determine winner of the game
 def compareMatchResults(teamScore, oponentScore, resultOptions):
@@ -62,7 +98,13 @@ def formatDate(date):
     return  d[2] + d[0] + d[1]
     
 
+
 #recordRacha(formatDate(args.date))
+#saveMatches()
+#print parsePredictionFile('../matches.txt')
+print recordPredictions('../matches.txt', formatDate(args.date))
+
+#print db.getPredictionId('P')
 
     
 
